c3396bd65b4709b0f0114b28c98604531309e587
Louis Phang <design.vincent.louis@gmail.com>
2020-10-06 18:07:58 +0800

    Starting point
    - Add README, with project instruction
    - Scaffold  basic project to run+build JavaScript Project using webpack
    - Install basic webpack dev dependencies and config

---
+++ README.md
@@ -0,0 +1,8 @@
+## Setup and Installation
+Before starting or building the project, please make sure your machine have NodeJS installed. Run `npm install` or `yarn` to install project dependencies.
+
+## Build
+Run `npm run build` or `yarn build` to have production codes in `/dist` folder.
+
+## Development
+Run `npm start` or `yarn start` to start development environment in your browser, by default your browser should open new tab with URL: http://localhost:8080/
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+const PATHS = {
+  dist: path.resolve(__dirname, "dist"),
+  contentBase: path.resolve(__dirname, "public"),
+};
+
+module.exports = (env = { NODE_ENV: "production" }) => {
+  const isDev = env.NODE_ENV === "local";
+  return {
+    mode: isDev ? "development" : "production",
+    entry: "./src/index.js",
+    devtool: isDev ? "cheap-source-map" : "source-map",
+    devServer: {
+      contentBase: PATHS.contentBase,
+      watchContentBase: true,
+    },
+    output: {
+      path: PATHS.dist,
+      filename: "[name].js",
+      publicPath: "/",
+    },
+  };
+};
