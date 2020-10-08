8b8d6ba48e8585c58376e6b80d83a091dd6acf94
Louis Phang <design.vincent.louis@gmail.com>
2020-10-08 17:20:24 +0800

    Drawing strike line
    - using existing state to draw a strike line when there is winner.

--- src/components/GameBoard/index.jsx
+++ src/components/GameBoard/index.jsx
@@ -36,6 +36,42 @@ const GameBoard = () => {
     setStrikeIndex(getStrike(newResult, currentMark));
     setStepCount(stepCount + 1);
   };
+  function renderStrikeLine() {
+    if (!winner) return null;
+
+    let top = "50%";
+    let left = "50%";
+    let transform = "translate(-50%, -50%)";
+
+    const [indexOne, indexTwo, indexThree] = strikeIndex;
+    const isRow = indexOne[0] === indexTwo[0] && indexTwo[0] === indexThree[0];
+    const isColumn =
+      indexOne[1] === indexTwo[1] && indexTwo[1] === indexThree[1];
+    const isDiagonal =
+      (indexOne[0] === indexThree[1] && indexTwo === "11") ||
+      (indexOne[0] === indexOne[1] &&
+        indexThree[0] === indexThree[1] &&
+        indexTwo === "11");
+
+    if (isRow) {
+      top = `${(100 / 3) * indexOne[0] + 100 / 6}%`;
+    } else if (isColumn) {
+      left = `${(100 / 3) * indexOne[1] + 100 / 6}%`;
+      transform = "translate(-50%, 0) rotate(-90deg)";
+    } else if (isDiagonal) {
+      transform = `translate(-50%, -50%) rotate(${
+        indexOne[1] === "0" ? "45" : "-45"
+      }deg) scaleX(1.4)`;
+    }
+
+    const lineClassnames = cx({
+      "gameboard__strike-line": true,
+      [`gameboard__strike-line--${winner}`]: !!winner,
+    });
+    const styles = { top, left, transform };
+
+    return <div className={lineClassnames} style={styles}></div>;
+  }
 
   function renderMessage() {
     const winnerIconClassnames = cx({
@@ -49,7 +85,9 @@ const GameBoard = () => {
       return (
         <p className="gameboard__message">
           <i className="icon icon--circle gameboard__message-icon gameboard__message-icon--o"></i>
-          <span className="gameboard__message-span">Game Over, and it is a tie</span>
+          <span className="gameboard__message-span">
+            Game Over, and it is a tie
+          </span>
           <i className="icon icon--cross gameboard__message-icon gameboard__message-icon--x"></i>
         </p>
       );
@@ -127,6 +165,7 @@ const GameBoard = () => {
         })}
         <div className="gameboard__horizontal-lines"></div>
         <div className="gameboard__vertical-lines"></div>
+        {renderStrikeLine()}
       </div>
     </>
   );
